bae0896dfd76704c9ba46acd78f8fe63f00a53ff
TonyChouteau <tony.chouteau@outlook.fr>
2020-01-24 18:19:26 +0100

    fixed multiplayer

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ var io = socketIO(server);
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "vps.tonychouteau.fr"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 // Routing
 app.get('/', function (request, response) {
@@ -30,29 +25,36 @@ server.listen(5000, function () {
 
 //Variables
 
-let x = 0;
-
-setInterval(function () {
-  io.sockets.emit('message', x);
-}, 1000);
-
-io.on('mouse', function () {
-  x++;
-  io.sockets.emit('message', 'Done');
-});
+let players = {};
 
 // Add the WebSocket handlers
 io.on('connection', function (socket) {
 
-  socket.on('mouse', function () {
-    x++;
-    console.log(x);
-    io.sockets.emit('message', 'Done');
+  socket.on('new', function () {
+    console.log("New player : "+socket.id)
+    players[socket.id] = {
+      x:450.0,
+      y:450.0,
+    }
   });
 
-  socket.on('new', function () {
-    x++;
-    console.log(x);
-    io.sockets.emit('message', x);
+  socket.on('move', function (data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 1;
+    }
+    if (data.up) {
+      player.y -= 1;
+    }
+    if (data.right) {
+      player.x += 2;
+    }
+    if (data.down) {
+      player.y += 2;
+    }
   });
 });
+
+setInterval(function() {
+  io.sockets.emit('state', players);
+}, 1000/60);
--- static/game.js
+++ static/game.js
@@ -1,19 +1,73 @@
-var socket = io("http://vps.tonychouteau.fr:5000");
+var socket = io("localhost:5000");
 
-socket.on('message', function (data) {
-    console.log(data);
+var movement = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+}
+
+document.addEventListener('keydown', function (event) {
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = true;
+            break;
+        case 87: // W
+            movement.up = true;
+            break;
+        case 68: // D
+            movement.right = true;
+            break;
+        case 83: // S
+            movement.down = true;
+            break;
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = false;
+            break;
+        case 87: // W
+            movement.up = false;
+            break;
+        case 68: // D
+            movement.right = false;
+            break;
+        case 83: // S
+            movement.down = false;
+            break;
+    }
+});
+
+socket.emit('new');
+
+setInterval(function() {
+  socket.emit('move', movement);
+}, 1000 / 60);
+
+socket.on('state', function(players) {
+    background(255);
+    fill(255,0,0);
+    ellipseMode(CENTER);
+    for (var id in players){
+        var player = players[id];
+        //console.log(player.x, player.y);
+        ellipse(player.x, player.y, 5, 5);
+    }
 });
 
 function setup() {
     createCanvas(900, 900);
     background(255);
 }
-
+/*
 function mousePressed() {
     console.log("Test");
     socket.emit('mouse');
-}
+}*/
 
-function draw() {
-
-}
+/*function draw() {
+    //ellipse(x, y, w, [h])
+}*/
